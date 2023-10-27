034664a51c9c4ce406bba4cad34951d9194a3296
isaacflower <isaacflower@gmail.com>
2023-10-27 10:57:50 +0100

    Added new on-plot parking module

---
+++ EvDemandModel/OnPlotParking/CalculateProportionOfVehiclesWithOnPlotParking.py
@@ -0,0 +1,23 @@
+from .AccommodationTypeCounter import AccommodationTypeCounter
+from .OnPlotParkingEstimator import OnPlotParkingEstimator
+from .VehiclesWithOnPlotParkingEstimator import VehiclesWithOnPlotParkingEstimator
+
+def calculate(
+        accomodation_type_2021_data,
+        house_2021_data,
+        car_van_2021_data
+        ):
+
+    accommodation_type_counter = AccommodationTypeCounter(accomodation_type_2021_data, house_2021_data)
+    accommodation_type_counts = accommodation_type_counter.count()
+
+    opp_estimator = OnPlotParkingEstimator(accommodation_type_counts, n_samples=1000)
+    opp_samples = opp_estimator.estimate()
+
+    vehicles_with_opp_estimator = VehiclesWithOnPlotParkingEstimator(opp_samples,
+                                                                     car_van_2021_data,
+                                                                     house_2021_data)
+
+    proportion_of_vehicles_with_opp = vehicles_with_opp_estimator.estimate()
+
+    return proportion_of_vehicles_with_opp
