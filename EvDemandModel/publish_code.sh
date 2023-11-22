8e28c37f669055cfca42cc4d3a4769bafc38d9f3
Rob Oldaker <roldaker@gmail.com>
2023-11-22 14:13:09 +0000

    Addedscripts to install EvDemand onto server. One does the python code the other the data.

---
+++ EvDemandModel/publish_code.sh
@@ -0,0 +1,45 @@
+#
+app="EvDemandModel"
+
+function raiseError()
+{
+    echo "$1";
+    echo "Press any key to continue";
+    read
+    exit -1;
+}
+
+usage="Usage:
+
+bash $0 [staging|production]
+"
+
+if [ $# -ne 1 ]; then
+    raiseError "$usage";
+fi 
+
+# Check params
+if [ "$1" = "staging" ]; then
+    dest="rob@odin.local"
+elif [ "$1" = "production" ]; then
+    dest="roberto@77.68.31.100"
+else
+    raiseError "$usage"
+fi
+
+echo "Zipping files .."
+pushd ..
+zip -r $app.zip EvDemandModel -x "EvDemandModel/EvDemandModel/Data/*" -x "*/__pycache__/*"
+popd
+
+echo "Copying to $dest"
+# sftp and ssh use key based authentication that needs setting up - see https://www.digitalocean.com/community/tutorials/how-to-configure-ssh-key-based-authentication-on-a-linux-server
+echo -e "cd websites/SmartEnergyLabDataApi\nput $app.zip" | sftp $dest
+echo "Installing on $dest"
+#
+ssh $dest "unzip -o websites/SmartEnergyLabDataApi/$app.zip -d websites/SmartEnergyLabDataApi/$app"
+
+echo "Publish successful. Press any key to continue";
+read
+
+
---
+++ EvDemandModel/publish_data.sh
@@ -0,0 +1,27 @@
+function raiseError()
+{
+    echo "$1";
+    echo "Press any key to continue";
+    read
+    exit -1;
+}
+
+usage="Usage:
+
+bash $0 [staging|production]
+"
+
+if [ $# -ne 1 ]; then
+    raiseError "$usage";
+fi 
+
+# Check params
+if [ "$1" = "staging" ]; then
+    dest="rob@odin.local"
+elif [ "$1" = "production" ]; then
+    dest="roberto@77.68.31.100"
+else
+    raiseError "$usage"
+fi
+
+rsync -aP ../EvDemandModel/EvDemandModel/Data -e 'ssh' $dest:~/websites/SmartEnergyLabDataApi/EvDemandModel/EvDemandModel/Data
